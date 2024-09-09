42f68331a8657352b6cd898a86d08034753e18d1
Hadrian Lazic <hadrian.lazic@gmail.com>
2024-09-08 20:53:20 -0700

    adding jump and roof

--- old/player.gd
+++ old/player.gd
@@ -2,11 +2,9 @@ extends RigidBody2D
 
 const jump_force = Vector2(0,-100)
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
